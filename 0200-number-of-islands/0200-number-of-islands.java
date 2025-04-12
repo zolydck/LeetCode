59a33fc187f7a363da9043bc4d142dd5131ffebf
zolydck <59064338+zolydck@users.noreply.github.com>
2025-04-12 16:14:52 -0500

    Time: 42 ms (5.22%), Space: 55.2 MB (5.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,48 @@
 class Solution {
-    int n,m;
+    int m,n;
+    int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
     public int numIslands(char[][] grid) {
         int count = 0;
         n = grid.length;
         if(n == 0) return 0;
         m = grid[0].length;
-        for(int i =0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
+
+        //Create a visited set
+        Set<String> visited = new HashSet<>();
+
+        for(int i= 0 ; i < n ; i++){
+            for(int j =0; j < m ; j++){
+                if(grid[i][j] == '1' && !visited.contains(i+","+j)){
                     ++count;
+                    bfs(i,j,grid,visited);
                 }
             }
         }
         return count;
     }
 
-    public void dfs(char[][] grid,int i , int j){
+    public void bfs(int i , int j, char[][] grid,Set<String> visited){
 
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j]!= '1') return;
-        grid[i][j] = '0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        //Add the row to visited
+        visited.add(i+","+j);
+        //Create a queue
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i,j});
+        while(!q.isEmpty()){
+            int[] point = q.poll();
+            int row = point[0];
+            int col = point[1];
+            for(int[] direction : directions){
+                int nrow = row + direction[0];
+                int ncol = col + direction[1];
+                if(nrow >=0 && ncol >=0 && nrow < n && ncol < m && grid[nrow][ncol] == '1' && !visited.contains(nrow+","+ncol)){
+                    q.add(new int[]{nrow,ncol});
+                    visited.add(nrow+ "," + ncol);
+
+                }
+            }
+        }
+
 
     }
 }
