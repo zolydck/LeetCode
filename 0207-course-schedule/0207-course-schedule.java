2a2a1da575ddf8b4332fd3a581246c9669f82dce
zolydck <59064338+zolydck@users.noreply.github.com>
2025-04-12 18:16:18 -0500

    Time: 6 ms (76.1%), Space: 45.2 MB (70.79%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        //create a adjacency list
+        ArrayList<Integer>[] alist = new ArrayList[numCourses];
+
+        //Create indegree array
+        int[] indegree = new int[numCourses];
+        List<Integer> ans = new ArrayList<>();
+        Arrays.fill(indegree,0);
+        for(int[] prereq: prerequisites){
+            int course = prereq[0];
+            int preCourse = prereq[1];
+
+            if(alist[preCourse] == null){
+                alist[preCourse] = new ArrayList<>();
+            }
+            alist[preCourse].add(course);
+            indegree[course]++;
+        }   
+
+        //Now the scheduling begins
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+                ans.add(i);
+            }
+        }  
+
+        //Now the execution begins
+        while(!q.isEmpty()){
+            int current = q.poll();
+            //Now iterate over the adjacency list
+            if(alist[current]!=null){
+                for(int i : alist[current]){
+                    indegree[i]--;
+                    if(indegree[i] == 0){
+                        q.offer(i);
+                        ans.add(i);
+                    }
+                }
+            }
+        }
+
+    return ans.size() == numCourses;
+    }
+}
