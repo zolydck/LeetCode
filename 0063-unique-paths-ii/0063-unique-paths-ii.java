ccdd805eeaa04a9dacf633f6baed03d1dbbc7340
zolydck <59064338+zolydck@users.noreply.github.com>
2025-08-13 11:46:49 -0500

    Time: 0 ms (100%), Space: 41.9 MB (37.98%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length,n = obstacleGrid[0].length;
+
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        obstacleGrid[0][0] = 1;
+        //fill first column
+        for(int i = 1 ; i < m ; i++){
+            obstacleGrid[i][0] = (obstacleGrid[i-1][0] == 0 || obstacleGrid[i][0] == 1) ? 0 : 1;
+        }
+        //fill first row
+        for(int i = 1 ; i < n ; i++){
+            obstacleGrid[0][i] = (obstacleGrid[0][i-1] == 0 || obstacleGrid[0][i] == 1) ? 0 : 1;
+        }
+
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = 0;
+                }
+                else{
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+                }
+            }
+        }
+        return obstacleGrid[m-1][n-1];
+        
+    }
+}
